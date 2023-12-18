3a6681a33a99cee376c9ae565aa0e689a4ccaf79
Rick South <rick@natamus.com>
2023-12-18 10:40:10 +0100

    (v3.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/biggerspongeabsorptionradius/util/Reference.java
+++ Common/src/main/java/com/natamus/biggerspongeabsorptionradius/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.biggerspongeabsorptionradius.util;
 public class Reference {
 	public static final String MOD_ID = "biggerspongeabsorptionradius";
 	public static final String NAME = "Bigger Sponge Absorption Radius";
-	public static final String VERSION = "3.2";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
